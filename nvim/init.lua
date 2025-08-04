cd468a5291fe1d29c528d016b6ab8082c3ae575d
Andreas Fladstad <andreasfladstad@Mac.home>
2025-08-04 11:07:55 +0200

    more stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,34 @@
-local vim = vim
 vim.loader.enable()
+local vim = vim
 local opt = vim.opt
 local map = vim.keymap.set
 local pack = vim.pack
+
+vim.g.mapleader = " "
+vim.g.localleader = " "
+
+map("n", "<Leader><cr>", ":noh<cr>", { silent = true })
+map({ "n", "v" }, "0", "^")
+
+map("n", ";", ":")
+map("n", ":", ";")
+
+map({ "n", "v" }, "{", "20gk", { silent = true })
+map({ "n", "v" }, "}", "20gj", { silent = true })
+map("t", "<esc>", "<C-\\><C-n>")
+
+-- Options
+opt.laststatus = 0
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.signcolumn = "yes"
+opt.clipboard = "unnamedplus"
+opt.completeopt = "fuzzy,noselect,menuone,popup"
+opt.swapfile = false
+opt.shell = "fish"
+opt.winborder = "bold"
+opt.complete = ".,o"
 
 pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
@@ -12,49 +38,23 @@ pack.add({
 	{ src = "https://github.com/catppuccin/nvim" },
 	{ src = "https://github.com/mattn/emmet-vim" },
 	{ src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope-fzy-native.nvim" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/davidosomething/format-ts-errors.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/saghen/blink.cmp", version = "v1.3.1" },
+	{ src = "https://github.com/ray-x/lsp_signature.nvim" },
+	{ src = "https://github.com/dmtrKovalenko/fff.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-	{ src = "https://github.com/echasnovski/mini.basics" },
 	{ src = "https://github.com/echasnovski/mini.surround" },
 	{ src = "https://github.com/echasnovski/mini.pairs" },
 })
 
 vim.cmd.colorscheme("catppuccin-mocha")
-
--- Options
-require("mini.basics").setup()
-opt.laststatus = 0
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.clipboard = "unnamedplus"
-opt.swapfile = false
-opt.shell = "fish"
-opt.winborder = "bold"
-
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "vimdoc", "tsx", "c", "typescript" },
-	highlight = {
-		enable = true,
-		use_languagetree = true,
-		additional_vim_regex_highlighting = true,
-	},
-	indent = { enable = true },
+	highlight = { enable = true },
 })
 
 vim.schedule(function()
-	local telescope = require("telescope")
-	telescope.setup({
-		defaults = {
-			border = false,
-		},
-	})
-	telescope.load_extension("fzy_native")
+	require("fzf-lua").setup({})
 
 	require("mason").setup()
 	require("mason-lspconfig").setup()
@@ -74,45 +74,7 @@ vim.schedule(function()
 			timeout_ms = 150,
 		},
 	})
-	require("blink-cmp").setup({
-		completion = {
-			accept = { auto_brackets = { enabled = true } },
-			list = { selection = { preselect = false, auto_insert = true } },
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 250,
-				treesitter_highlighting = true,
-			},
-		},
-		keymap = {
-			["<C-CR>"] = { "accept", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-			["<C-up>"] = { "scroll_documentation_up", "fallback" },
-			["<C-down>"] = { "scroll_documentation_down", "fallback" },
-		},
-
-		signature = { enabled = true },
-		cmdline = { enabled = false },
-		sources = {
-			default = { "lsp", "path", "buffer" },
-			providers = {
-				lsp = {
-					min_keyword_length = 0,
-					score_offset = 9,
-				},
-				path = {
-					score_offset = 10,
-					min_keyword_length = 0,
-				},
-				buffer = {
-					min_keyword_length = 2,
-					score_offset = -5,
-					max_items = 5,
-				},
-			},
-		},
-	})
+	require("lsp_signature").setup()
 	require("oil").setup({
 		default_file_explorer = true,
 		view_options = {
@@ -137,27 +99,19 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
+	callback = function(args, bufnr)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		client.server_capabilities.semanticTokensProvider = nil
+		vim.lsp.completion.enable(true, client.id, bufnr, {
+			autotrigger = true,
+		})
 	end,
 })
 
 -- Keymaps
-map("n", "<Leader><cr>", ":noh<cr>", { silent = true })
-map({ "n", "v" }, "0", "^")
-
-map("n", ";", ":")
-map("n", ":", ";")
-
-map({ "n", "v" }, "{", "20gk", { silent = true })
-map({ "n", "v" }, "}", "20gj", { silent = true })
-map("t", "<esc>", "<C-\\><C-n>")
-
-map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-map("n", "<leader>e", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-map("n", "<leader>f", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-map("n", "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-map("n", "<leader>r", "<cmd>lua require('telescope.builtin').resume()<cr>")
+map("n", "<leader>e", "<cmd>FzfLua files<cr>")
+map("n", "<leader>f", "<cmd>FzfLua live_grep<cr>")
+map("n", "<leader>b", "<cmd>FzfLua buffers<cr>")
+map("n", "<leader>r", "<cmd>FzfLua resume<cr>")
 map("n", "-", "<cmd>Oil<cr>", { silent = true })
